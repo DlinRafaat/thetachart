cdf84c842cefc776deccd767f095baeda1033690
Dlin Rafaat <dlin.rafaat@hotmail.com>
2026-03-27 15:00:59 -0500

    removed caching for get_price

--- src/stock_price_lookup.py
+++ src/stock_price_lookup.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import streamlit as st
 
-@st.cache_data(ttl=60)
+
 def get_price(pick_ticker):
     ticker = yf.Ticker(pick_ticker.upper().strip())
     return ticker.fast_info["last_price"]
